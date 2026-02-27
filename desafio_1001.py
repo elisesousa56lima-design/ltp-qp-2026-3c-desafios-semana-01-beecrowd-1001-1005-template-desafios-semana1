7c41ef11b41a7e75cd80ce44a22f96dc5ad7ba13
elisesousa56lima-design <elisesousa56lima@gmail.com>
2026-02-27 08:26:52 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -1,4 +1,4 @@
-"""
+7"""
 Beecrowd 1001 - Extremamente Básico
 
 Leia 2 valores inteiros e armazene-os nas variáveis A e B.
@@ -12,3 +12,18 @@ você receberá "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
 # Escreva sua solução abaixo
+# Lê as duas notas como ponto flutuante (double precision)
+A = float(input())
+B = float(input())
+
+# Pesos: A = 3.5, B = 7.5
+# Soma dos pesos = 3.5 + 7.5 = 11.0
+peso_A = 3.5
+peso_B = 7.5
+
+# Cálculo da média ponderada
+media = (A * peso_A + B * peso_B) / 11.0
+
+# Imprime "MEDIA =" com 5 casas decimais
+print(f"MEDIA = {media:.5f}")
+
