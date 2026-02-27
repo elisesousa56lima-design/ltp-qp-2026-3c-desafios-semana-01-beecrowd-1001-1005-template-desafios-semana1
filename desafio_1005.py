5d21084b789d087e7f6acee96fdac2ef6b3c9b68
elisesousa56lima-design <elisesousa56lima@gmail.com>
2026-02-27 08:31:52 -0300

    Update desafio_1005.py

--- desafio_1005.py
+++ desafio_1005.py
@@ -18,3 +18,17 @@ de imprimir o fim de linha após o resultado, caso contrário, você receberá
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1005
 
 # Escreva sua solução abaixo
+# Lê as duas notas como ponto flutuante (double precision)
+A = float(input())
+B = float(input())
+
+# Pesos: A = 3.5, B = 7.5
+# Soma dos pesos = 3.5 + 7.5 = 11.0
+peso_A = 3.5
+peso_B = 7.5
+
+# Cálculo da média ponderada
+media = (A * peso_A + B * peso_B) / 11.0
+
+# Imprime "MEDIA =" com 5 casas decimais
+print(f"MEDIA = {media:.5f}")
